6e283df0f782c472727ee4ab12b298490e805066
Hp-pc <Hp-pc@hp>
2017-06-20 01:05:39 +0530

    final

---
+++ MavenProjectDemo/src/Object_Repo/myhcl_homefirst.java
@@ -0,0 +1,14 @@
+package Object_Repo;
+
+public class myhcl_homefirst {
+
+	public String hometab=".//*[@id='nav']/div/ul/li[1]/a";
+	public String hclicon=".//*[@id='main']/div[1]/div[1]/div";
+	public String settingSpan=".//*[@id='setting']/span";
+	public String Logout=".//*[@id='acnt-info']/li[4]/a";
+	
+	/**
+	 * @param args
+	 */
+	
+}
